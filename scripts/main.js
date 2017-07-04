88c98491d150e7ad47b28062f4d66561b644ed34
aniket965 <aniket965.as@gmail.com>
2017-07-04 15:58:24 +0530

    added Social links !

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+    initializeSocialLinks();
+
+})
+
+function initializeSocialLinks() {
+    $('.fa-facebook').click(function () {
+        window.open("https://www.facebook.com/aniket.sharma.583671", "facebook");
+    });
+    $('.fa-twitter').click(function () {
+        window.open("https://twitter.com/aniket965as", "twitter");
+    });
+    $('.fa-rss').click(function () {
+        window.open("http://aniket965.tech/blog", "blog");
+    });
+    $('.fa-linkedin').click(function () {
+        window.open("https://www.linkedin.com/in/aniket-sharma-548413131/", "linkedin");
+    });
+    $('.fa-medium').click(function () {
+        window.open("https://medium.com/@aniket965", "medium");
+    });
+
+}
+
